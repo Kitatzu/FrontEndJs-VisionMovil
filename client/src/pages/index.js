b8f8068dbcd4aa18eb3e407ec1a2d574f117c089
Exequiel Rodriguez <exe922@gmail.com>
2023-04-10 15:08:53 -0300

    NavBar basic

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,8 @@
+import NavBar from "@/components/NavBar";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1> Home </h1>;
+  return <NavBar />;
 }
