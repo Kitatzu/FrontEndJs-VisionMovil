9c8394c122d9a511650ac5ddb2dd185d22829013
cajadios <santi-ventu@hotmail.com>
2023-04-16 13:05:35 -0300

    inputes test

--- client/src/pages/register/client.js
+++ client/src/pages/register/client.js
@@ -1,12 +1,16 @@
 import React from "react";
 import UserName from "./Inputs/userNameInput";
 import Email from "./Inputs/emailInput"
+import CountryInput from "./Inputs/countryInput";
+import Phone from "./Inputs/phoneInput";
 
 export default function Client(){
   return(
     <div>
     <UserName />
     <Email />
+    <CountryInput />
+    <Phone />
     </div>
   )
 }
