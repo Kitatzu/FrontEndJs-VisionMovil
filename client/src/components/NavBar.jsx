fe31f3106aadb999bb9420e7f3254a55d328b712
valcoellar <valcoellar@gmail.com>
2023-06-14 12:24:28 -0600

    responsive NavBar+Menu

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import { AppBar, Container, Grid, Toolbar, Typography} from '@mui/material';
 
 import Link from "next/link"
 import Image from 'next/image'
 
 
+
 import stylos from './NavBar.module.css';
 import Box from '@mui/material/Box';
 import visionLogo from './images/asset5.png'
 
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export default function NavBar () {
+
+  const [visible, setVisible] = useState(false)
 
 
  
@@ -19,39 +26,79 @@ export default function NavBar () {
     <nav className={stylos.MenuBar}>
       <ul>
 		<li>
-<Image src={ visionLogo } alt="My Image"  width={ 400 } height={70} />
+<Image src={ visionLogo } alt="My Image"  width={ 400 } height={70}/>
 		</li>
-
+<div className={stylos.burgerMenuItems}>
+<li>
+   <MenuIcon className={stylos.burgerMenu} onClick={() => setVisible(!visible)} />
+   <SearchIcon className={stylos.burgerMenu}/>
+   <ShoppingCartIcon className={stylos.burgerMenu}/>
+</li>
+</div>
  <div  className={stylos.MenuOptions}>     
+        
+
         <li>
-          <Link href="/about">
+          <Link href="/about" className={stylos.MenuOption}>
             Quienes Somos
           </Link>
         </li>
         <li>
-          <Link href="/servicios">
+          <Link href="/servicios" className={stylos.MenuOption} >
             Servicios 
           </Link>
         </li>
         <li>
-          <Link href="/categorias">
+          <Link href="/categorias" className={stylos.MenuOption}>
             Categorias 
           </Link>
         </li>
         <li>
-            <Link href= '/tienda'> 
+            <Link href= '/tienda' className={stylos.MenuOption}> 
             Tienda
             </Link>
         </li>
         <li>
-            <Link href= '/login'>
+            <Link href= '/login' className={stylos.MenuOption}>
                  Mi cuenta 
             </Link>
         </li>
-</div>
-
+ </div>
       </ul>
     </nav>
+
+{visible ? <div >
+
+        <div  className={stylos.MenuOptionsVertical}>     
+                       <li>
+                  <Link href="/about" className={stylos.verticalMenu}>
+                    Quienes Somos
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/servicios" className={stylos.verticalMenu}>
+                    Servicios 
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/categorias" className={stylos.verticalMenu}>
+                    Categorias 
+                  </Link>
+                </li>
+                <li>
+                    <Link href= '/tienda' className={stylos.verticalMenu}> 
+                    Tienda
+                    </Link>
+                </li>
+                <li>
+                    <Link href= '/login' className={stylos.verticalMenu}>
+                         Mi cuenta 
+                    </Link>
+                </li>
+         </div>
+
+</div> : null}
+
 </AppBar>
   )
 }
